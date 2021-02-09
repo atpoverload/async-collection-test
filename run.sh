685d73df97db25fc8cb3fac16355467d2db3e108
timurbey <atpoverload@gmail.com>
2021-02-08 21:17:50 -0700

    change to run script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-periods=(4000000 1000000 500000 100000 50000 1)
+periods=(1000000)
 for period in "${periods[@]}"; do
   bazel run src:scheduling_benchmark --jvmopt="-Dcollector.period=$period"
   bazel run src:sleeping_benchmark --jvmopt="-Dcollector.period=$period"
